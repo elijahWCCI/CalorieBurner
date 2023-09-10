3ea04fe49fb1fb51daaa4d6f4b875073355723e0
elijahWCCI <elijahgrantham@wecancodeit.org>
2023-09-10 15:03:04 -0400

    Build

---
+++ Java/src/main/java/com/wcci/calorieburner/CalorieburnerApplication.java
@@ -0,0 +1,13 @@
+package com.wcci.calorieburner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieburnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieburnerApplication.class, args);
+	}
+
+}
