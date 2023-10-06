a3f8b54ad0637f592cc56756e228c0eb27cb9135
Juan C Alvarado Rivera <juanc2719@hotmail.com>
2023-10-05 20:35:09 -0400

    discard

--- Java/src/main/java/com/wcci/calorieburner/Repositories/ExerciseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wcci.calorieburner.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wcci.calorieburner.Models.ExerciseModel;
-
-public interface ExerciseRepository extends JpaRepository<ExerciseModel, Long> {
-    ExerciseModel findByName(String name);
-}
--- Java/src/main/java/com/wcci/calorieburner/Repositories/SelectedFoodRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wcci.calorieburner.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.wcci.calorieburner.Models.SelectedFood;
-
-public interface SelectedFoodRepository extends CrudRepository<SelectedFood, Long> {
-}
